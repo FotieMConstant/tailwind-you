51d85d5488e66ba6c010958f0d40b81711d0bd61
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-03-22 11:15:39 +0300

    doc init

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Tailwind You</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/FotieMConstant/tailwind-you',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/FotieMConstant/tailwind-you/docs',
   footer: {
     text: 'Nextra Docs Template',
   },
