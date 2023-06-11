d0e9fcf7e4c671a9f437332878365abaa9c8351c
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-06-11 17:44:18 +0300

    🛠setup project with storybook + tailwind 3.x

--- packages/vue/lib/src/index.js
+++
@@ -1,7 +0,0 @@
-import LearnVueVButton from "./components/LearnVueVButton.vue";
-
-export default{
-    install: (app, options) =>{
-        app.component("LearnVueVButton", LearnVueVButton)
-    }
-}
