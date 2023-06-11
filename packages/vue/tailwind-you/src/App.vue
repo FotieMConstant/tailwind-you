a1eebd858326edcb6e6f2a683db5bffa4e49a7a6
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2023-06-11 20:11:29 +0300

    chore(docs): rename package name from nextra-docs-template to tailwind-you-docs chore(docs): update repository url to point to the new repository chore(docs): update bugs url to point to the new repository chore(docs): update homepage url to point to the new repository feat(docs): add warning callout to the about page
    feat(docs): add comment for banner component
    chore(docs): update footer text in theme.config.tsx
    
    chore(vue): remove HelloWorld component and .DS_Store file, update App.vue to remove unused code and add welcome message

--- packages/vue/tailwind-you/src/App.vue
+++ packages/vue/tailwind-you/src/App.vue
@@ -1,18 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    Welcome to Tailwind-You
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup>
+</script>
 
 <style scoped>
 .logo {
